dad42a7b81e6e13b2eda37ca48364ce3b045ca40
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-12 16:34:54 +0300

    feat: up

--- src/components/fileSystem.js
+++ src/components/fileSystem.js
@@ -1,6 +1,17 @@
 import {fileURLToPath} from "url";
 import path from "path";
 import os from 'os';
+import {stdout} from "process";
 
 export const getHomeDir = () => os.homedir();
 export const getCurrentPathName = () => path.dirname(fileURLToPath(import.meta.url));
+export const goUp = (currentPath) => {
+  let newPath = currentPath.split(path.sep);
+  if (newPath.length > 1) {
+    newPath.pop();
+    newPath = newPath.join(path.sep);
+  }
+  else newPath = currentPath;
+  stdout.write(`You are currently in ${newPath}\n`);
+  return newPath;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import {startFileManager, exitFileManager} from "./components/startExit.js";
 import readline from "readline";
-import {getCurrentPathName, getHomeDir} from "./components/fileSystem.js";
+import {getCurrentPathName, getHomeDir, goUp} from "./components/fileSystem.js";
 import {stdin, stdout} from "process";
 import {showListOfCommands} from "./components/commands.js";
 
 export const fileManager = async () => {
   const userName = await startFileManager();
-  const currentPath = getCurrentPathName();
+  let currentPath = getHomeDir();
   const rl = readline.createInterface({
     input: stdin,
     output: stdout
   })
-  rl.setPrompt(`You are currently in ${getHomeDir()}\n`);
+  rl.setPrompt(`You are currently in ${currentPath}\n`);
   rl.prompt();
   rl.on('SIGINT', () => exitFileManager(userName));
   rl.on('line', (input) => {
@@ -24,6 +24,31 @@ export const fileManager = async () => {
       case '.exit' :
         exitFileManager(userName);
         break;
+
+      case 'up' :
+        currentPath = goUp(currentPath);
+        break;
+      case 'cd' :
+        break;
+      case 'add' :
+        break;
+      case 'rn' :
+        break;
+      case 'cp' :
+        break;
+      case 'mv' :
+        break;
+      case 'rm' :
+        break;
+      case 'os' :
+        break;
+      case 'hash' :
+        break;
+      case 'compress' :
+        break;
+      case 'decompress' :
+        break;
+
       default :
         stdout.write(`There is no command "${command}". To see available commands type "?". Go ahead and try again\n`);
     }
